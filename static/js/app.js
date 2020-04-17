dd8c5ca8c0cabb61cca9c03f65b7c2836b8593e1
Your Name <you@example.com>
2020-04-18 01:56:36 +0300

    supporting hebrew

--- static/js/app.js
+++ static/js/app.js
@@ -31,7 +31,12 @@ function escapeHtml(unsafe) {
          .replace(/>/g, "&gt;")
          .replace(/"/g, "&quot;")
          .replace(/'/g, "&#039;");
- }
+}
+
+function hasHebrew(text) {
+    var hebrew = /[\u0590-\u05FF]/;
+    return hebrew.test(text);
+};
 
 function renderHTML(data) {
     var articlesRow = document.getElementById('articles');
@@ -41,7 +46,12 @@ function renderHTML(data) {
         htmlString += '<h5 class="display-5" align="center" >' + key + '</h5>'
         for (var i = 0; i < data[key].length; i++) {
             var article = data[key][i]
-            htmlString += '<div class="row"><div class="col col-md-9" style="border-bottom: 1px solid #eeeded;">'
+            if (hasHebrew(article.headline)) {
+                htmlString += '<div class="row"><div class="col col-md-9 hebrew" style="border-bottom: 1px solid #eeeded;">'
+            }
+            else {
+                htmlString += '<div class="row"><div class="col col-md-9" style="border-bottom: 1px solid #eeeded;">'
+            }
             if (article.summary != '') {
                 htmlString += '<a style="text-decoration: none; color: black;" href="' + article.link + '" target="_blank" data-toggle="tooltip" data-placement="bottom" data-html="true" title="<em>'
                 htmlString += escapeHtml(article.summary) + '</em>">' + escapeHtml(article.headline) + '</a>'
