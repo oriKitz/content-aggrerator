e390a7e271a9e37431ac93ec50c706aa2e01fc59
Your Name <you@example.com>
2020-04-21 18:11:57 +0300

    Added user management: registration, login and logout

--- aggregator/views.py
+++ aggregator/views.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import render_template, url_for, request, jsonify, redirect, flash
 import datetime
 from collections import defaultdict
 import re
-from aggregator import app
-from .models import NewsItem
+from aggregator import app, db, bcrypt
+from .models import NewsItem, User
 from sqlalchemy import or_
+from flask_login import login_user, current_user, logout_user, login_required
+from aggregator.forms import RegistrationForm, LoginForm
 
 
 @app.route('/')
-def main():
-    return render_template('home.html')
+@app.route('/home')
+def home():
+    return render_template('news.html', inner_title='News Portal')
 
 
 @app.route('/search')
@@ -35,3 +38,46 @@ def get_data(text_limit, use_regex):
     for article in matching_articles:
         news_items[article.website].append(article.serialize())
     return {k: v[:8] for k, v in news_items.items()}
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
