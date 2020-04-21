f78365bdf39c9bae2f92ddff5dec3887311ca787
Your Name <you@example.com>
2020-04-21 17:11:30 +0300

    Added User object and migration

--- aggregator/models.py
+++ aggregator/models.py
@@ -1,6 +1,7 @@
-from aggregator import db
+from aggregator import db, login_manager
 import datetime
 from sqlalchemy.inspection import inspect
+from flask_login import UserMixin
 
 
 class NewsItem(db.Model):
@@ -16,3 +17,19 @@ class NewsItem(db.Model):
 
     def serialize(self):
         return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"'{self.username}', '{self.email}'"
